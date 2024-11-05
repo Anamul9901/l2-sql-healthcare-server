78920e865bc65cd933522e34361ddcf91a00a65a
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-05 13:18:27 +0600

    reusable serarch

--- src/app/modules/Admin/admin.service.ts
+++ src/app/modules/Admin/admin.service.ts
@@ -5,28 +5,23 @@ const prisma = new PrismaClient();
 const getAllFromDb = async (params: any) => {
   const andConditions: Prisma.AdminWhereInput[] = [];
 
+
+  const adminSearcgAbleFirlds = ["name", "email"] 
   if (params.searchTerm) {
     andConditions.push({
-      OR: [
-        {
-          name: {
-            contains: params?.searchTerm,
-            mode: "insensitive",
-          },
+      OR: adminSearcgAbleFirlds.map((field) => ({
+        [field]: {
+          contains: params?.searchTerm,
+          mode: "insensitive",
         },
-        {
-          email: {
-            contains: params?.searchTerm,
-            mode: "insensitive",
-          },
-        },
-      ],
+      })),
     });
   }
 
-  // console.dir(andConditions, {depth: 'indinity'})
+//   console.dir(andConditions, {depth: 'indinity'})
 
   const whereContitions: Prisma.AdminWhereInput = { AND: andConditions };
+  //   console.log({whereContitions})
 
   const result = await prisma.admin.findMany({
     where: whereContitions,
