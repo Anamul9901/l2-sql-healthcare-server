ae753ac71039bb18d18fe5cf918221e681e6bfcf
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-05 12:59:38 +0600

    Basic search empliment

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -2,12 +2,21 @@ import { Request, Response } from "express";
 import { AdminService } from "./admin.service";
 
 const getAllFromDB = async (req: Request, res: Response) => {
-  const result = await AdminService.getAllFromDb();
-  res.status(200).json({
-    success: true,
-    message: "Admin data retrieve",
-    data: result,
-  });
+  // console.log(req.query)
+  try {
+    const result = await AdminService.getAllFromDb(req.query);
+    res.status(200).json({
+      success: true,
+      message: "Admin data retrieve",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.name || "Something went wrong",
+      error: err,
+    });
+  }
 };
 
 export const AdminController = {
