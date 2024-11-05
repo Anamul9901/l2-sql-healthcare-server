4433377abf2012bb2d9019c636a9b1824108816a
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-05 12:52:02 +0600

    Admin find all

--- src/app/modules/User/user.controller.ts
+++ src/app/modules/User/user.controller.ts
@@ -2,8 +2,21 @@ import { Request, Response } from "express";
 import { userService } from "./user.service";
 
 const createAdmin = async (req: Request, res: Response) => {
-  const result = await userService.createAdmin(req.body);
-  res.send(result);
+  try {
+    const result = await userService.createAdmin(req.body);
+    // res.send(result);
+    res.status(200).json({
+      success: true,
+      message: "Admin created successfuly!",
+      data: result,
+    });
+  } catch (err: any) {
+    res.status(500).json({
+      success: false,
+      message: err.name || "Something went wrong",
+      error: err,
+    });
+  }
 };
 
 export const userController = {
