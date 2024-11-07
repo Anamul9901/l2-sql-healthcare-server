8bda4d8274ddde47b335bc18f00a51aa648c9fa3
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-07 12:44:42 +0600

    Handle Not found rout error

--- src/app.ts
+++ src/app.ts
@@ -16,8 +16,23 @@ app.get("/", (req: Request, res: Response) => {
   });
 });
 
+// 1s e ai rout e chack korbe, error asle neser rout e jabe
 app.use("/api/v1", router);
 
+// uporer rout e error asle ai route e ase error show korbe. ai route e o error asle neser rout e jabe
 app.use(globalErrorHandler);
+
+// uporer rout e error asa mane user wrong rout e hite korse. mane 'rout not found' error dekhate hobe.
+app.use((req: Request, res: Response, next: NextFunction) => {
+  // console.log(req)
+  res.status(404).json({
+    success: false,
+    message: "API NOT FOUND!",
+    error: {
+      path: req.originalUrl,
+      message: "Your requested path is not found!",
+    },
+  });
+});
 
 export default app;
