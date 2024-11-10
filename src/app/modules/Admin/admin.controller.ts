0c5cf9f3395f223239fec8f0721405cfe67afafe
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-10 09:50:48 +0600

    Reusable catchAsync function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -3,16 +3,8 @@ import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../../shared/sendResponse";
+import catchAsync from "../../../shared/catchAsync";
 
-const catchAsync = (fn: RequestHandler) => {
-  return async (req: Request, res: Response, next: NextFunction) => {
-    try {
-      await fn(req, res, next);
-    } catch (err) {
-      next(err);
-    }
-  };
-};
 
 // RequestHandler lekle r req, res, next er type lekte hobe nah.
 const getAllFromDB: RequestHandler = catchAsync(async (req, res) => {
@@ -57,45 +49,28 @@ const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
   });
 });
 
-const deleteFromDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const result = await AdminService.deleteFromDB(id);
+const deleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AdminService.deleteFromDB(id);
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin data deleted!",
-      data: result,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin data deleted!",
+    data: result,
+  });
+});
+const softDeleteFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AdminService.softDeleteFromDB(id);
 
-const softDeleteFromDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const result = await AdminService.softDeleteFromDB(id);
-
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin data deleted!",
-      data: result,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin data deleted!",
+    data: result,
+  });
+});
 
 export const AdminController = {
   getAllFromDB,
--- src/app/modules/Admin/admin.routes.ts
+++ src/app/modules/Admin/admin.routes.ts
@@ -1,14 +1,19 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { AdminController } from "./admin.controller";
 
 const router = express.Router();
+
+const validateRequest = (req: Request, res: Response, next: NextFunction) => {
+  console.log("req", req.body);
+  next();
+};
 
 router.get("/", AdminController.getAllFromDB);
 
 router.get("/:id", AdminController.getByIdFromDB);
 
 // put method: jode thake tahole update kore dey, jode na thake tahole create kore dey
-router.patch("/:id", AdminController.updateIntoDB);
+router.patch("/:id", validateRequest, AdminController.updateIntoDB);
 
 router.delete("/:id", AdminController.deleteFromDB);
 
