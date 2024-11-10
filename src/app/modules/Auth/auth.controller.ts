b7e9c68e9b9348ee1584239541937516974587ba
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-10 10:55:27 +0600

    Add refresh token and set cookie

--- src/app/modules/Auth/auth.controller.ts
+++ src/app/modules/Auth/auth.controller.ts
@@ -6,11 +6,24 @@ import sendResponse from "../../../shared/sendResponse";
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const result = await AuthService.loginUser(req.body);
 
+  const {refreshToken} = result;
+
+
+  // refresh toke cookie te set kore debo, fole eita sequre thake, keu read korte parbe nah
+  res.cookie('refreshToken', refreshToken, {
+    secure: false, // production e true hobe must
+    httpOnly: true
+  })
+
+
   sendResponse(res, {
     statusCode: 200,
     success: true,
     message: "Logged in successfully!",
-    data: result,
+    data: {
+        accessToken: result.accessToken,
+        needPasswordChange: result.needPasswordChange,
+    }
   });
 });
 
--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -14,6 +14,9 @@ const loginUser = async (payload: { email: string; password: string }) => {
     userData.password
   );
   //   console.log({isCorrectPassword})
+  if (!isCorrectPassword) {
+    throw new Error("Password incorrect!");
+  }
 
   const accessToken = jwt.sign(
     {
@@ -23,11 +26,28 @@ const loginUser = async (payload: { email: string; password: string }) => {
     "secrate_key",
     {
       algorithm: "HS256",
-      expiresIn: "15m",
+      expiresIn: "5m",
     }
   );
-  console.log({ accessToken });
-  return accessToken;
+
+  const refreshToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "secrate_key_refresh",
+    {
+      algorithm: "HS256",
+      expiresIn: "30d",
+    }
+  );
+
+//   console.log({ accessToken });
+  return {
+    accessToken,
+    refreshToken,
+    needPasswordChange: userData.needPasswordChange,
+  };
 };
 
 export const AuthService = {
