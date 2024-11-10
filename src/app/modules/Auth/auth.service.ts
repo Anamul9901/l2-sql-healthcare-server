74e21965b7576e637b84c2531ca2d9c6eb892772
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-10 10:44:45 +0600

    Create jwt token

--- src/app/modules/Auth/auth.service.ts
+++ src/app/modules/Auth/auth.service.ts
@@ -1,6 +1,33 @@
+import prisma from "../../../shared/prisma";
+import * as bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
+
 const loginUser = async (payload: { email: string; password: string }) => {
-  console.log({payload})
-    console.log("user logged in...");
+  const userData = await prisma.user.findUniqueOrThrow({
+    where: {
+      email: payload.email,
+    },
+  });
+
+  const isCorrectPassword: boolean = await bcrypt.compare(
+    payload.password,
+    userData.password
+  );
+  //   console.log({isCorrectPassword})
+
+  const accessToken = jwt.sign(
+    {
+      email: userData.email,
+      role: userData.role,
+    },
+    "secrate_key",
+    {
+      algorithm: "HS256",
+      expiresIn: "15m",
+    }
+  );
+  console.log({ accessToken });
+  return accessToken;
 };
 
 export const AuthService = {
