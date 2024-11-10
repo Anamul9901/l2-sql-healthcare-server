c29a4dd8a331948ba6ff64a6db8405ff278a57c0
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-10 09:40:48 +0600

    Add catchAsync or hireOrder function

--- src/app/modules/Admin/admin.controller.ts
+++ src/app/modules/Admin/admin.controller.ts
@@ -1,73 +1,61 @@
-import { NextFunction, Request, Response } from "express";
+import { NextFunction, Request, RequestHandler, Response } from "express";
 import { AdminService } from "./admin.service";
 import pick from "../../../shared/pick";
 import { adminFilterableFields } from "./admin.constant";
 import sendResponse from "../../../shared/sendResponse";
 
-const getAllFromDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
+const catchAsync = (fn: RequestHandler) => {
+  return async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      await fn(req, res, next);
+    } catch (err) {
+      next(err);
+    }
+  };
+};
+
+// RequestHandler lekle r req, res, next er type lekte hobe nah.
+const getAllFromDB: RequestHandler = catchAsync(async (req, res) => {
   // console.log(req.query)
-  try {
-    const filters = pick(req.query, adminFilterableFields);
-    const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
-    // console.log(options);
-    const result = await AdminService.getAllFromDb(filters, options);
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin data retrieve",
-      meta: result.meta,
-      data: result.data,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+  const filters = pick(req.query, adminFilterableFields);
+  const options = pick(req.query, ["limit", "page", "sortBy", "sortOrder"]);
+  // console.log(options);
+  const result = await AdminService.getAllFromDb(filters, options);
 
-const getByIdFromDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const result = await AdminService.getByIdFromDB(id);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin data retrieve",
+    meta: result.meta,
+    data: result.data,
+  });
+});
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Admin data retrieve by id",
-      data: result,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+const getByIdFromDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const result = await AdminService.getByIdFromDB(id);
 
-const updateIntoDB = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  try {
-    const { id } = req.params;
-    const data = req.body;
-    const result = await AdminService.updateIntoDB(id, data);
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Admin data retrieve by id",
+    data: result,
+  });
+});
 
-    sendResponse(res, {
-      statusCode: 200,
-      success: true,
-      message: "Updated successfully",
-      data: result,
-    });
-  } catch (err: any) {
-    next(err);
-  }
-};
+const updateIntoDB = catchAsync(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const data = req.body;
+  const result = await AdminService.updateIntoDB(id, data);
+
+  sendResponse(res, {
+    statusCode: 200,
+    success: true,
+    message: "Updated successfully",
+    data: result,
+  });
+});
 
 const deleteFromDB = async (
   req: Request,
