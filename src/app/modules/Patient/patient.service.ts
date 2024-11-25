5c8424dc96f11c702688dd31c9a3da0205caf921
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-25 16:42:41 +0600

    Create patient healthe Data & medical report with patient update

--- src/app/modules/Patient/patient.service.ts
+++ src/app/modules/Patient/patient.service.ts
@@ -95,20 +95,56 @@ const getByIdFromDB = async (id: string): Promise<Patient | null> => {
 };
 
 const updateIntoDB = async (id: string, payload: any) => {
-  console.log(id, payload);
-  await prisma.patient.findUniqueOrThrow({
-    where: { id, isDeleted: false },
+  const { patientHealthData, medicalReport, ...patientData } = payload;
+
+  const patientInfo = await prisma.patient.findUniqueOrThrow({
+    where: {
+      id,
+      isDeleted: false,
+    },
   });
-  const result = await prisma.patient.update({
-    where: { id },
-    data: payload,
+
+  const result = await prisma.$transaction(async (transactionClient) => {
+    // update patient data
+    const updatePatient = await transactionClient.patient.update({
+      where: { id },
+      data: patientData,
+      include: {
+        patientHealthData: true,
+        medicalReport: true,
+      },
+    });
+
+    //create or update patient health data
+    if (patientHealthData) {
+      const healthData = await transactionClient.patientHealthData.upsert({
+        where: {
+          patientId: patientInfo.id,
+        },
+        update: patientHealthData,
+        create: { ...patientHealthData, patientId: patientInfo.id },
+      });
+    }
+
+    // create medical report data
+    if (medicalReport) {
+      const report = await transactionClient.medicalReport.create({
+        data: { ...medicalReport, patientId: patientInfo.id },
+      });
+    }
+  });
+
+  const responseData = await prisma.patient.findUnique({
+    where: {
+      id: patientInfo.id,
+    },
     include: {
       patientHealthData: true,
       medicalReport: true,
     },
   });
 
-  return result;
+  return responseData;
 };
 
 export const PatientService = {
