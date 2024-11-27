68ce045b7caceb30cd87c3aaaea21a69d070d41d
Anamul Haque <anamulhaque9901@gmail.com>
2024-11-27 19:15:21 +0600

    Appointment model create

--- src/app/modules/DoctorSchedul/doctorSchedul.service.ts
+++ src/app/modules/DoctorSchedul/doctorSchedul.service.ts
@@ -30,6 +30,92 @@ const insertIntoDB = async (
   return result;
 };
 
+const getAllSchedule = async (
+  filters: any,
+  options: IPaginationOptions,
+) => {
+  const { startDate, endDate, ...filterData } = filters;
+
+  const { limit, page, skip, sortBy, sortOrder } =
+    paginationHelper.calculatePaginatin(options);
+
+  const andConditions: Prisma.DoctorSchedulesWhereInput[] = [];
+
+  if (startDate && endDate) {
+    andConditions.push({
+      AND: [
+        {
+          schedule: {
+            startDateTime: {
+              gte: startDate,
+            },
+          },
+        },
+        {
+          schedule: {
+            endDateTime: {
+              lte: endDate,
+            },
+          },
+        },
+      ],
+    });
+  }
+
+  //   console.log(Object.keys(filterData)); // aikhane key gulu array akare debe
+  if (Object.keys(filterData).length > 0) {
+    // conver string to boolien of isBooked filter
+    if (
+      typeof filterData.isBooked === "string" &&
+      filterData.isBooked === "true"
+    ) {
+      filterData.isBooked = true;
+    } else if (
+      typeof filterData.isBooked === "string" &&
+      filterData.isBooked === "false"
+    ) {
+      filterData.isBooked = false;
+    }
+
+    andConditions.push({
+      AND: Object.keys(filterData).map((key) => ({
+        [key]: {
+          equals: (filterData as any)[key],
+        },
+      })),
+    });
+  }
+
+  //   console.dir(andConditions, {depth: 'indinity'})
+
+  const whereContitions: Prisma.DoctorSchedulesWhereInput =
+    andConditions.length > 0 ? { AND: andConditions } : {};
+
+  const result = await prisma.doctorSchedules.findMany({
+    where: whereContitions,
+    skip,
+    take: limit,
+    orderBy:
+      sortBy && sortOrder
+        ? {
+            // [sortBy]: sortOrder,
+          }
+        : {},
+  });
+
+  const total = await prisma.doctorSchedules.count({
+    where: whereContitions,
+  });
+  return {
+    meta: {
+      page,
+      limit,
+      total,
+    },
+    data: result,
+  };
+};
+
 const getMySchedule = async (
   filters: any,
   options: IPaginationOptions,
@@ -168,5 +254,6 @@ const deleteFromDB = async (user: IAuthUser, scheduleId: string) => {
 export const DoctorSchedulService = {
   insertIntoDB,
   getMySchedule,
+  getAllSchedule,
   deleteFromDB,
 };
